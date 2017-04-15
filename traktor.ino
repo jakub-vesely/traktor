30dbef4e01669b2c9c0a84db0362d6681620c929
Jakub Veselý <jakub_vesely@seznam.cz>
2017-04-15 16:31:18 +0200

    adjustments for a new PCB with a new MPU6050

--- traktor.ino
+++ traktor.ino
@@ -7,15 +7,20 @@
 
 #define NUMBER_BUFFER_SIZE 8
 #define PRESSURE_OFFSET 99 //teoretically 102
- 
-MPU6050 accelerometr;
-#define X_COMP 2740 //compensation
-#define Y_COMP 14900
-#define Z_COMP -200
+#define BUTTONS_PIN 7 //A7
+#define PRESSURE_PIN 0 //A0
+#define LED_PIN 6 //D6
 
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+MPU6050 accelerometr;
+int MPUOffsets[6] = { -707, -3158, 952, 167, 54,  -258 }; //source: https://forum.arduino.cc/index.php?action=dlattach;topic=446713.0;attach=193816
+LiquidCrystal lcd(12, 10, 5, 4, 3, 2);
 #define ROWS 2
 #define COLUMNS 16
+
+int mode = 0;
+#define MAX_MODE 4
+bool pressed = false;
 
 struct Eeprom
 {
@@ -23,11 +28,23 @@ struct Eeprom
   int16_t yDegreeComp; //y
   int16_t pressureOffset; //o
   int16_t pressureDivider; //d
+  int16_t positionCount; //c
+  int16_t multiplier0; //1
+  int16_t multiplier1; //1
+  int16_t multiplier2; //2
+  int16_t multiplier3; //3
+  int16_t multiplier4; //4
+  int16_t multiplier5; //5
+  int16_t multiplier6; //6
+  int16_t multiplier7; //7
+  int16_t multiplier8; //8
+  int16_t multiplier9; //9
+  int16_t limit;
 } eeprom;
 
 void fillAngles(int &xAngle, int &yAngle)
 {
-  const int maxCount = 200;
+  const int maxCount = 100;
   
   int16_t ax, ay, az;
   long ax_p = 0;
@@ -43,11 +60,12 @@ void fillAngles(int &xAngle, int &yAngle)
     ay_p = ay_p + ay;
     az_p = az_p + az;
   }
-     
-  x = ax_p / maxCount + X_COMP;
-  y = ay_p / maxCount + Y_COMP;
-  z = az_p / maxCount + Z_COMP;
-     
+    
+  x = ax_p / maxCount;
+  y = ay_p / maxCount;
+  z = az_p / maxCount;
+
+    
   xAngle = (int)(atan2(x, sqrt(square(y) + square(z)))/(M_PI/180)) + eeprom.xDegreeComp;
   yAngle = (int)(atan2(y, sqrt(square(x) + square(z)))/(M_PI/180)) + eeprom.yDegreeComp;  
 }
@@ -64,15 +82,22 @@ void ResetEeprom()
 void setup() {
     Wire.begin();
     accelerometr.initialize();
-    //TODO
-    //if (accelerometr.testConnection())
-    //  Serial.println("Spojeni OK...");
+    accelerometr.setXAccelOffset(MPUOffsets[0]);
+    accelerometr.setYAccelOffset(MPUOffsets[1]);
+    accelerometr.setZAccelOffset(MPUOffsets[2]);
+    accelerometr.setXGyroOffset(MPUOffsets[3]);
+    accelerometr.setYGyroOffset(MPUOffsets[4]);
+    accelerometr.setZGyroOffset(MPUOffsets[5]);
       
     lcd.begin(COLUMNS, ROWS);
 
-    //ResetEeprom();
     EEPROM.get(0, eeprom);
     Serial.begin(115200);
+    while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB
+    }
+
+    pinMode(LED_PIN, OUTPUT);
 }
 
 float getPressure()
@@ -80,12 +105,12 @@ float getPressure()
   /* 5V/1024bit = 4.883 mV/bit
    * offset_bit = 0 MPa  ... 0.5V  ... 102 bit  \
    * range_bit =                                 > 820 bit 
-   * max_bit =    16MPa ... 4.5V  ... 922 bit  /
+   * max_bit =    16MPa ... 4.5V  ... 922 bit   /
    * 
    * p(Mpa) / range_MPa = (analog_bit - offset_bit) / range_bit;
    * p(Mpa) = (analog_bit - offset_bit) * range_MPa / range_bit; 
    */
-  return ((float)analogRead(1) - PRESSURE_OFFSET + eeprom.pressureOffset) * 16 / (820 + eeprom.pressureDivider);
+  return ((float)analogRead(PRESSURE_PIN) - PRESSURE_OFFSET + eeprom.pressureOffset) * 16 / (820 + eeprom.pressureDivider);
 } 
 
 void displayValue(float value, int row, int right, int maxDigit, int decimalCount)
@@ -174,34 +199,83 @@ void ProcessCommand()
   if (Serial.available()) 
   {
     char command = Serial.read();
-    char decider = Serial.read();
-    bool setValue = (decider != '?');
-    switch (command)
+    if (command == 'r')
     {
-      case 'r':
-        ResetEeprom();
-        Serial.write("EEPROM reseted");
-      break;
-      case 'x':
-        _ProcessCommand(command, setValue, eeprom.xDegreeComp);
-      break;
-      case 'y':
-        _ProcessCommand(command, setValue, eeprom.yDegreeComp);
-      break;
-      case 'o':
-        _ProcessCommand(command, setValue, eeprom.pressureOffset);
-      break;
-      case 'd':
-        _ProcessCommand(command, setValue, eeprom.pressureDivider);
-      break;
+      ResetEeprom();
+      Serial.write("EEPROM reseted\n");
+      return;
+    }
+
+    bool setValue = true;
+    if (command == '?')
+    {
+      if (!Serial.available())
+      {
+         Serial.write("Commands:\n");
+         Serial.write("x - xDegreeComp\n");
+         Serial.write("y - yDegreeComp\n");
+         Serial.write("o - pressureOffset\n");
+         Serial.write("d - pressureDivider\n");
+         Serial.write("c - positionCount\n");
+         Serial.write("0 - multiplier0\n");
+         Serial.write("...\n");
+         Serial.write("9 - multiplier9\n");
+         return;
+      }
+      command = Serial.read();
+      setValue = false;
+    }
+    Serial.read(); //expected '='
+    switch (command)
+    {   
+      case 'x': _ProcessCommand(command, setValue, eeprom.xDegreeComp); break;
+      case 'y': _ProcessCommand(command, setValue, eeprom.yDegreeComp); break;
+      case 'o': _ProcessCommand(command, setValue, eeprom.pressureOffset); break;
+      case 'd': _ProcessCommand(command, setValue, eeprom.pressureDivider); break;
+      case 'c': _ProcessCommand(command, setValue, eeprom.positionCount); break;
+      case '0': _ProcessCommand(command, setValue, eeprom.multiplier0); break;
+      case '1': _ProcessCommand(command, setValue, eeprom.multiplier1); break;
+      case '2': _ProcessCommand(command, setValue, eeprom.multiplier2); break;
+      case '3': _ProcessCommand(command, setValue, eeprom.multiplier3); break;
+      case '4': _ProcessCommand(command, setValue, eeprom.multiplier4); break;
+      case '5': _ProcessCommand(command, setValue, eeprom.multiplier5); break;
+      case '6': _ProcessCommand(command, setValue, eeprom.multiplier6); break;
+      case '7': _ProcessCommand(command, setValue, eeprom.multiplier7); break;
+      case '8': _ProcessCommand(command, setValue, eeprom.multiplier8); break;
+      case '9': _ProcessCommand(command, setValue, eeprom.multiplier9); break;
       default:
-        SendValue(command, 0, false);  
-    }      
+        Serial.write("Unknown command");
+        while (Serial.available())
+        {
+          Serial.read(); //I have to clear all followed letters after unknown command 
+        }
+      break;
+    }     
+  }
+}
+int getWeight()
+{
+  switch (mode)
+  {
+    case 0: return getPressure() * eeprom.multiplier0;
+    case 1: return getPressure() * eeprom.multiplier1;
+    case 2: return getPressure() * eeprom.multiplier2;
+    case 3: return getPressure() * eeprom.multiplier3;
+    case 4: return getPressure() * eeprom.multiplier4;
+    case 5: return getPressure() * eeprom.multiplier5;
+    case 6: return getPressure() * eeprom.multiplier6;
+    case 7: return getPressure() * eeprom.multiplier7;
+    case 8: return getPressure() * eeprom.multiplier8;
+    case 9: return getPressure() * eeprom.multiplier9;
   }
 }
 void loop() {
     int xAngle, yAngle;
     fillAngles(xAngle, yAngle);
+    //SendValue('x', xAngle);
+    //SendValue('y', yAngle);
+    //delay(100);
+    
     displayValue(xAngle, 0, 2, 3, 0);
     lcd.print("| ");
     displayValue(yAngle, 1, 2, 3, 0);
@@ -209,9 +283,37 @@ void loop() {
     
     displayValue(getPressure(), 0, 12, 6, 1);
     lcd.print("MPa");
+
+    displayValue(getWeight(), 1, 11, 6, 0);
+    lcd.print("kg");
+
+    displayValue(mode + 1, 1, 15, 1, 0);
     
     //lcd.print(getPressure());
     lcd.print("   ");
 
-    ProcessCommand();
+    ProcessCommand(); 
+    
+    if (300 < analogRead(BUTTONS_PIN)) //second should be > 500
+    {
+      digitalWrite(LED_PIN, HIGH);
+      if (!pressed)
+      {
+        pressed = true;
+        mode++;
+        if (mode == eeprom.positionCount)
+          mode = 0;
+      }
+    }
+    else
+    {
+      digitalWrite(LED_PIN, LOW);
+      pressed = false;
+    }
+    
+    /*if (mode == 0)
+      digitalWrite(11, HIGH);
+    else
+      digitalWrite(11, LOW);
+   */   
 }
